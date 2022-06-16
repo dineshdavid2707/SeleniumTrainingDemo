be0d5cb149f2232a685bda7afdef258b6446a8b6
dineshdavid2707 <dineshdavid1995@gmail.com>
2022-06-16 16:50:02 +0530

    Updated Script for WebOrder.java

--- src/test/java/pk_OrangeHRM/WebOrders.java
+++ src/test/java/pk_OrangeHRM/WebOrders.java
@@ -1,1 +1,44 @@
 //Pls add code for web order
+package pk_OrangeHRM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebOrders {
+  @Test
+  public void WeborderLoginVerification() {
+	        //Launch Chrome
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			//Enter URL
+			driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx");
+			//Enter User Login Details
+			driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+			driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+			//Click Login Btn
+			driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+			//Exp Result : Verify User landed in DashBoard
+			if(driver.findElement(By.linkText("Order")).isDisplayed()) {
+				System.out.println("Login Successfully");
+			}
+			else {
+				System.out.println("Login Failed");
+			}
+			
+			//Click Logout
+			driver.findElement(By.linkText("Logout")).click();
+			
+			if(driver.findElement(By.id("ctl00_MainContent_login_button")).isDisplayed()) {
+				System.out.println("Logout Successfully");
+			}
+			else {
+				System.out.println("Logout Failed");
+			}
+			//Close Browser
+			driver.quit();
+  }
+}
